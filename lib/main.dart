e208f27ee0a75c082ed6161e8d1f9c0409e3ab53
Pratik Fandade <fandadep6@gmail.com>
2024-11-09 22:03:10 -0500

    This is just to construct auth0 further

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-// import 'bottom_bar.dart';
-// import 'image.dart';
-// import 'session.dart';
 import 'login.dart';
 import 'package:rive/rive.dart';
 
@@ -12,6 +9,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

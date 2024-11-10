3be09b74c76b1143fcd3645982ea93af3e9b31f0
Pratik Fandade <fandadep6@gmail.com>
2024-11-09 22:40:10 -0500

    Just the code Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'login.dart';
-import 'package:rive/rive.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:pablotheraphy/pablo_theraphy.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pablo Therapy',
-      theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: 'Geist',
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0x00000000),
-          title: const Text(
-            'Pablo Therapy'
-          ),
-        ),
-        extendBodyBehindAppBar: true,
-        body: Stack(
-          children: [
-            const RiveAnimation.asset(
-              'images/background.riv',
-              fit: BoxFit.cover,
-            ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    'images/home.svg',
-                    width: 200,
-                    height: 200,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(31),
-                    child: Builder(
-                      builder: (context) => const NameDialog(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+void main() async {
+  await dotenv.load();
+  runApp(const PablosTheraphyApp());
 }
